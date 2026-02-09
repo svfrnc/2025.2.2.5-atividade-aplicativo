37eebc874a138c6adfd74cbc15a3475a52487079
Sávio França <164408156+svfrnc@users.noreply.github.com>
2026-02-09 17:48:22 +0000

    versão final sem comentários

--- script.js
+++ script.js
@@ -2,25 +2,19 @@ let executouCalculo = false;
 function inserir(valor) {
     var visor = document.getElementById('resultado');
     
-    // Lista de operadores para checagem
     const operadores = ['+', '-', '*', '/'];
-
-    // Se o visor mostra um resultado e o usuário clica em um NÚMERO
     if (executouCalculo && !operadores.includes(valor)) {
         visor.value = valor; // Substitui o resultado pelo novo número
     } else {
         visor.value += valor; // Se for operador, continua a conta
     }
     
-    // Resetamos o estado para que os próximos cliques funcionem normalmente
     executouCalculo = false;
 }
-// Função para limpar o visor (botão C)
 function limpar() {
     document.getElementById('resultado').value = "";
     executouCalculo = false; 
 }
-// Função que realiza o cálculo
 function calcular() {
     var visor = document.getElementById('resultado');
     var expressao = visor.value;
@@ -30,7 +24,6 @@ function calcular() {
             var resultadoFinal = eval(expressao);
             visor.value = resultadoFinal;
             
-            // ESSA LINHA É A CHAVE: Ela avisa que o visor agora tem um resultado
             executouCalculo = true; 
             
             salvarNoHistorico(expressao, resultadoFinal);
@@ -53,10 +46,7 @@ function efeitoMusical() {
 
         visor.value = "365365365365365365365365365365365365";
         visor.classList.add('visor-animado');
-        // Ativa a animação no fundo
         corpoPagina.classList.add('fundo-animado');
-
-        // Ativa a animação nos botões
         botoes.forEach(function(botao) {
             botao.setAttribute('data-original', botao.innerHTML);
             botao.innerHTML = "brat";
@@ -69,10 +59,8 @@ function efeitoMusical() {
         visor.value = "";
         visor.classList.remove('visor-animado');
 
-        // Remove a animação do fundo
         corpoPagina.classList.remove('fundo-animado');
 
-        // Remove a animação dos botões
         botoes.forEach(function(botao) {
             var textoOriginal = botao.getAttribute('data-original');
             botao.innerHTML = textoOriginal;
@@ -95,7 +83,6 @@ function pressionartecla(event) {
     }
     
 }
-// Adicione estas variáveis e funções ao seu script.js
 
 function alternarHistorico() {
     const aba = document.getElementById('aba-historico');
@@ -116,7 +103,6 @@ function atualizarExibicaoHistorico() {
     const lista = document.getElementById('lista-historico');
     const historico = JSON.parse(localStorage.getItem('historico_brat')) || [];
     
-    // Limpa a lista atual para reconstruir (Retrieve/HTML dinâmico)
     lista.innerHTML = "";
 
     historico.forEach(item => {
